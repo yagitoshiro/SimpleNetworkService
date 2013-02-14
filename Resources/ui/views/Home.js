91283e080b3743f922333145e561d48901e7060c
yagitoshiro <yagitoshiro@gmail.com>
2013-02-14 16:37:37 +0900

    add table view

--- Resources/ui/views/Home.js
+++ Resources/ui/views/Home.js
@@ -1,13 +1,33 @@
-function Home(){
+function Home(parentWindow){
+  var Cloud = require('ti.cloud');
+  var account = require('lib/account').account;
   var self = Ti.UI.createView();
 
-  var label = Ti.UI.createLabel({
-    text:"イエーイ、ホーム画面",
-    width: Ti.UI.SIZE,
-    height: Ti.UI.SIZE
+  var data = [];
+  var table = Ti.UI.createTableView({
+    data: data
   });
-  self.add(label);
 
+  function loadData(){
+    if(Ti.App.Properties.hasProperty('username')){
+      Cloud.debug = true;
+      Cloud.Users.login(account.login_params(), function(e){
+        Cloud.Posts.query({}, function(e){
+          var data = [];
+          var posts = e.posts;
+          var length = posts.length;
+          for(var i = 0; i < length; i++){
+            data.push({title: posts[i].title, data: posts[i]});
+          }
+          table.setData(data);
+        });
+      });
+    }
+  }
+  parentWindow.addEventListener('focus', function(e){
+    loadData();
+  });
+  self.add(table);
   return self;
 }
 
